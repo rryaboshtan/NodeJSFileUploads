b69d15342152036718c1222a6aa0691d7074f51a
rryaboshtan <rrvworkroman@gmail.com>
2021-12-17 02:20:35 +0200

    add server.js

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const app = express();
-
 const path = require('path');
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// const hbs = require('express-handlebars');
+
+app.set('view engine', 'ejs');
+// app.engine(
+//    'hbs',
+//    hbs({
+//       extname: 'hbs',
+//       defaultLayout: 'layout',
+//       defaultView: 'default',
+//       layoutsDir: path.join(__dirname, 'views'),
+//       partialsDir: path.join(__dirname, 'views/partials'),
+//    })
+// );
+// app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+   res.send('<h3>Express server</h3>');
+});
+
+app.listen(3000, () => console.log('Server is started on http://localhost:3000'));
