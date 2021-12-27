c3836e7bd879eaa6c93ed2b35f1786e548852d76
rryaboshtan <rrvworkroman@gmail.com>
2021-12-27 03:22:00 +0200

    add multer

---
+++ server/middleware/multer.js
@@ -0,0 +1,16 @@
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+   destination: (req, file, cb) => {
+      cb(null, 'uploads');
+   },
+   filename: (req, file, cb) => {
+      let ext = file.originalname.substring(file.originalname.lastIndexOf('.'));
+
+      cb(null, `${file.filename}-${Date.now()}${ext}`);
+   },
+});
+
+const store = multer({ storage });
+
+module.exports = store;
